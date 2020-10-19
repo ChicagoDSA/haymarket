f6a41d0f70164f7fdd7c2842c5c49633cde85558
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-10-18 19:02:26 -0500

    basic functionality working

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,34 +1,34 @@
 window.onload = function() {
 	var mouseOver = false;
-	var menuIcon = document.getElementById("menu-icon");
-	var menuContent = document.getElementById("menu-content");
+	var mobileMenuIcon = document.getElementById("mobile-menu-icon");
+	var mobileMenuContent = document.getElementById("mobile-menu-content");
 	var nameLink = document.getElementById("name-link");
 	
-	if (menuIcon && menuContent) {
+	if (mobileMenuIcon && mobileMenuContent) {
 		// Show menu when icon is moused over
-		menuIcon.onfocus = function () {
-			showMenu(menuContent);
+		mobileMenuIcon.onfocus = function () {
+			showMenu(mobileMenuContent);
 		};
 
 		// Hide menu when chapter name is focused
 		nameLink.onfocus = function () {
-			hideMenu(menuContent);
+			hideMenu(mobileMenuContent);
 		};
 
 		// Set mouseover flags
-		menuContent.onmouseover = function () {
+		mobileMenuContent.onmouseover = function () {
 			mouseOver = true;
 		};
-		menuContent.onmouseout = function () {
+		mobileMenuContent.onmouseout = function () {
 			mouseOver = false;
 		};
 
 		// When the mouse moves
 		window.onmousemove = function () {
 			// If it's outside the visible menu
-			if (menuContent.classList.contains("visible-menu") && mouseOver == false) {
+			if (mobileMenuContent.classList.contains("visible-menu") && mouseOver == false) {
 				// Hide the menu
-				menuContent.classList.remove("visible-menu");
+				mobileMenuContent.classList.remove("visible-menu");
 			};
 		};
 	};	
